4507f821ab10bf9b88a139a214f86b9538f0f791
Patricia <TrishS7777@gmail.com>
2024-11-09 01:02:58 -0500

    Caesar Cipher code works - cannot resolve import pytest

--- cipher.py
+++ cipher.py
@@ -1,1 +1,26 @@
 # add your code here
+
+abc = "abcdefghijklmnopqrstuvwxyz"
+
+shift = 5
+#shift = int(input("Please enter the number of places to shift: "))
+index = abc.find("z")
+index = (index + shift) % 26
+char = abc[index]
+#print(char)
+
+
+original_text = input("Please enter a  sentence: ")
+original_text = original_text.lower()
+
+
+encrypted_text = ""
+for char in original_text:
+    if char in abc:
+        index = abc.find(char)
+        index = (index + shift) % 26
+        char = abc[index]
+    encrypted_text += char
+
+print(original_text)
+print (encrypted_text)
